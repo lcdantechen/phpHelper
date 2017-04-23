908a1bc0ac22bb7aacf91349e82f81d46113ee1b
lcdantechen <lcdantechen@gmail.com>
2017-04-23 18:52:41 -0400

    update

--- changeFormURLinContactForm7.php
+++ changeFormURLinContactForm7.php
@@ -1,4 +1,4 @@
-<!--  The fact that you were able to get it so quickly is wonderful, and shows the amount of research effort you're putting into this.
+<!--  This helps to change the form url in contact form plugin, on all pages or just one single page. The fact that you were able to get it so quickly is wonderful, and shows the amount of research effort you're putting into this.
 
 Put all of this in functions.php-->
 
